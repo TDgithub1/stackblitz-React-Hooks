f00125167645b1ca424e50307fd9ef313e3096ac
TDgithub1 <105603327+TDgithub1@users.noreply.github.com>
2023-07-21 19:24:33 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,49 +1,46 @@
 import React from 'react';
 import './style.css';
-import React, { useRef, useState } from 'react';
+import React, { useMemo,useState } from 'react';
+//useMemo hook use to speed up app
+
+const inputHandle= (inputValue) =>{
+
+  let data
+
+  for(let i=0;i<100000;i++){
+    data = i
+  }
+
+  return inputValue
+
+}
 
 export default function App() {
-  const [myData, setMyData] = useState({
-    image: '',
-    name: '',
-    city: '',
-    position: '',
-  });
-
-  const imageUrlRef = useRef(); //this hook did not rerender
-  const nameRef = useRef();
-  const cityRef = useRef();
-  const positionRef = useRef();
-
-  const setDataHandle = () => {
-    setMyData({
-      image: imageUrlRef.current.value,
-      name: nameRef.current.value,
-      city: cityRef.current.value,
-      position: positionRef.current.value,
-    });
-
-    imageUrlRef.current.focus(); // after click curser send to image text box
-  };
-
+  const [imageUrl, setImageUrl] = useState("");
+  const [name, setName] = useState("");
+ 
+  const imageData = useMemo(()=> inputHandle(setImageUrl),[imageUrl]);
+ 
+ 
   console.log('rerender');
 
+ 
   return (
     <div>
-      <input type="text" placeholder="Enter image Url" ref={imageUrlRef} />
+      <input type="text" placeholder="Enter image Url" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
 
-      <input type="text" placeholder="Enter your name" ref={nameRef} />
+      <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)}/>
 
-      <input type="text" placeholder="Enter your city" ref={cityRef} />
+      <input type="text" placeholder="Enter your city" />
 
-      <input type="text" placeholder="Enter your position" ref={positionRef} />
+      <input type="text" placeholder="Enter your position" />
 
-      <button onClick={setDataHandle}>Set Data</button>
+      <button onClick={}>Set Data</button>
 
-      <p>{myData?.name}</p>
-      <p>{myData?.image}</p>
-      <p>{myData?.city}</p>
-      <p>{myData?.position}</p>
+      <p>{imageUrl}</p>
+      <p>{name}</p>
+      <p>{imageData}</p>
+      
 
       <div></div>
     </div>
