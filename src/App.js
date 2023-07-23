dd0625800954f538602119d9bb7e2c5ce46dd6a7
TDgithub1 <105603327+TDgithub1@users.noreply.github.com>
2023-07-23 18:49:22 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
 import React from 'react';
 import './style.css';
-import React, { useMemo,useState } from 'react';
-//useMemo hook use to speed up app
+import React, { useCallback, useEffect, useState } from 'react';
+//useCallback hook can indentify function differance/return function
+//useMemo return value
 
-const inputHandle= (inputValue) =>{
-
-  let data
-
-  for(let i=0;i<100000;i++){
-    data = i
-  }
-
-  return inputValue
-
-}
+//both use for app optimize
 
 export default function App() {
-  const [imageUrl, setImageUrl] = useState("");
-  const [name, setName] = useState("");
- 
-  const imageData = useMemo(()=> inputHandle(setImageUrl),[imageUrl]);
- 
- 
-  console.log('rerender');
+  const [imageUrl, setImageUrl] = useState('');
+  const [name, setName] = useState('');
+  const [result, setResult] = useState('');
 
- 
+  const myFunction = useCallback(
+    () => () => {
+      return 'amila';
+    },
+    []
+  );
+
+  console.log('rerender');
+  console.log(result);
+
+  useEffect(() => {
+    console.log('useEffect');
+    console.log(myFunction());
+    setResult(myFunction());
+  }, [myFunction]);
+
   return (
     <div>
-      <input type="text" placeholder="Enter image Url" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+      <input
+        type="text"
+        placeholder="Enter image Url"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
 
-      <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <input type="text" placeholder="Enter your city" />
 
       <input type="text" placeholder="Enter your position" />
 
-      <button onClick={}>Set Data</button>
+      <button>Set Data</button>
 
       <p>{imageUrl}</p>
       <p>{name}</p>
-      <p>{imageData}</p>
-      
+      <p></p>
 
       <div></div>
     </div>
