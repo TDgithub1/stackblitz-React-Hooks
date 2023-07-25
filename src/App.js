239a4d228e2e9a021f191f8492dd1011e496afd7
TDgithub1 <105603327+TDgithub1@users.noreply.github.com>
2023-07-25 17:34:53 +0530

    1 file modified, 3 deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './style.css';
-import { createContext, useState } from 'react';
-import Com_f from './Components/Com_f';
+// import React from 'react';
+// import './style.css';
+// import { createContext, useState } from 'react';
+// import Com_f from './Components/Com_f';
 
-//use for user data,such as password,email
+// //use for user data,such as password,email
 
-export const userAuth = createContext({});
+// export const userAuth = createContext({});
 
-export default function App() {
-  const [username, setUsername] = useState('');
+// export default function App() {
+//   const [username, setUsername] = useState('');
 
-  return (
-    <userAuth.Provider
-      value={{
-        username,
-        setUsername,
-      }}
-    >
-      <h1>App {username}</h1>
-      <Com_f />
-    </userAuth.Provider>
-  );
-}
+//   return (
+//     <userAuth.Provider
+//       value={{
+//         username,
+//         setUsername,
+//       }}
+//     >
+//       <h1>App {username}</h1>
+//       <Com_f />
+//     </userAuth.Provider>
+//   );
+// }
