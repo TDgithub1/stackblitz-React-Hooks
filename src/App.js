7fcdd7d1aacb9837eb0b570667045fa65490affb
TDgithub1 <105603327+TDgithub1@users.noreply.github.com>
2023-08-03 20:24:54 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,101 +1,47 @@
 import React from 'react';
 import './style.css';
-import React, { useReducer } from 'react';
+import React, {
+  useState,
+  useTransition,
+  useDeferredValue,
+  useEffect,
+} from 'react';
 
-const reducer = (state, action) => {
-  if (action.type === 'setImage') {
-    return { ...state, image: action.data };
-  } else if (action.type === 'setName') {
-    return { ...state, name: action.data };
-  } else if (action.type === 'setCity') {
-    return { ...state, city: action.data };
-  } else if (action.type === 'setPosition') {
-    return { ...state, position: action.data };
-  } else if (action.type === 'setArray') {
-    return { ...state, dataArray: [...state.dataArray, action.data] };
-  } else {
-    return state;
-  }
-};
+//useTransition & useDeferredValue use for chose first run code part
 
 export default function App() {
-  const [state, setState] = useReducer(reducer, {
-    name: '',
-    image: '',
-    city: '',
-    position: '',
-    dataArray: [],
-  }); //react hook(can use differant name for reducer/reducer = function name)
+  const [inputVal, setInpuVal] = useState('');
+  const [data, setData] = useState([]);
 
-  console.log(state);
+  const finalInputVal = useDeferredValue(inputVal); //wait for change inputval then pass to data to value
+
+  const [loading, delayFun] = useTransition();
+
+  console.log(`inputVal is ${inputVal} and finalInputVal is ${finalInputVal}`);
+
+  useEffect(() => {
+    delayFun(() => {
+      setData([]);
+
+      fetch('https://jsonplaceholder.typicode.com/comments')
+        .then((response) => response.json())
+        .then((json) => setData(json));
+    });
+  }, [inputVal]);
+
+  const inputHandle = (e) => {
+    setInpuVal(e.target.value);
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter image Url"
-        value={state.image}
-        onChange={(e) =>
-          setState({
-            type: 'setImage',
-            data: e.target.value,
-          })
-        }
-      />
+      <input type="text" value={inputVal} onChange={inputHandle} />
 
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={state.name}
-        onChange={(e) =>
-          setState({
-            type: 'setName',
-            data: e.target.value,
-          })
-        }
-      />
+      {loading ? <h1>Loading</h1> : null}
 
-      <input
-        type="text"
-        placeholder="Enter your city"
-        value={state.city}
-        onChange={(e) =>
-          setState({
-            type: 'setCity',
-            data: e.target.value,
-          })
-        }
-      />
-
-      <input
-        type="text"
-        placeholder="Enter your position"
-        value={state.position}
-        onChange={(e) =>
-          setState({
-            type: 'setPosition',
-            data: e.target.value,
-          })
-        }
-      />
-
-      <button
-        onClick={(e) =>
-          setState({
-            type: 'setArray',
-            data: {
-              image: state.image,
-              name: state.name,
-              city: state.city,
-              position: state.position,
-            },
-          })
-        }
-      >
-        Set Data
-      </button>
-
-      <div>Set data div</div>
+      {data?.map(({ name }, index) => (
+        <h2 key={index}>{name}</h2>
+      ))}
     </div>
   );
 }
